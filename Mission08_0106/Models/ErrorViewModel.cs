45dfe9279d917e3d2b41bcf6f079572d7c016e4e
WillardRich <wrichar4@byu.edu>
2026-02-25 09:35:12 -0700

    Add project files.

---
+++ Mission08_0106/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_0106.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
