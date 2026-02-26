f3325012055474a5e446e31f7dff51f3f212b5c9
WillardRich <wrichar4@byu.edu>
2026-02-26 09:09:07 -0700

    database model first update

---
+++ Mission08_0106/Models/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Mission08_0106.Models;
+
+public interface ITaskRepository
+{
+    IQueryable<TaskItem> Tasks { get; }
+    IQueryable<Category> Categories { get; }
+
+    void AddTask(TaskItem task);
+    void UpdateTask(TaskItem task);
+    void DeleteTask(TaskItem task);
+
+    void Save();
+}
